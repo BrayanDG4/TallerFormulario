0e4627795e31ff47d5c7f8187b08c7b5bf888dc5
Juan Sebastian Badillo Rincón <71675425+Juan7742@users.noreply.github.com>
2022-11-12 15:50:45 -0500

    Update NavBar.jsx

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
       <header className="banner p-10 border-b bg-white shadow mb-2">
             <div className="container mx-auto flex justify-between items-center">
                 <nav className="flex gap-3 items-center">
-                    
+                    EQUPO ROCKET
                 </nav>
             </div>
       </header>
